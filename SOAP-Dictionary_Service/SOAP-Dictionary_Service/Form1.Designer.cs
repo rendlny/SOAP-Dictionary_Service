63c8e0dea2cf775025a2d5ea4b1a2f29b736690b
Lauren Delaney <ren.delaney@gmail.com>
2016-11-28 23:06:16 +0000

    Nearly Complete, Need to make definition print string and not location

--- SOAP-Dictionary_Service/SOAP-Dictionary_Service/Form1.Designer.cs
+++ SOAP-Dictionary_Service/SOAP-Dictionary_Service/Form1.Designer.cs
@@ -28,65 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnGetDefinition = new System.Windows.Forms.Button();
+            this.txtWord = new System.Windows.Forms.TextBox();
+            this.txtDefintion = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.labelPageNum = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // btnGetDefinition
             // 
-            this.button1.Location = new System.Drawing.Point(76, 51);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(117, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnGetDefinition.Location = new System.Drawing.Point(76, 51);
+            this.btnGetDefinition.Name = "btnGetDefinition";
+            this.btnGetDefinition.Size = new System.Drawing.Size(117, 23);
+            this.btnGetDefinition.TabIndex = 0;
+            this.btnGetDefinition.Text = "Get Definition";
+            this.btnGetDefinition.UseVisualStyleBackColor = true;
+            this.btnGetDefinition.Click += new System.EventHandler(this.btnGetDefinition_Click);
             // 
-            // textBox1
+            // txtWord
             // 
-            this.textBox1.Location = new System.Drawing.Point(35, 25);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(200, 20);
-            this.textBox1.TabIndex = 1;
+            this.txtWord.Location = new System.Drawing.Point(35, 25);
+            this.txtWord.Name = "txtWord";
+            this.txtWord.Size = new System.Drawing.Size(200, 20);
+            this.txtWord.TabIndex = 1;
             // 
-            // textBox2
+            // txtDefintion
             // 
-            this.textBox2.Location = new System.Drawing.Point(12, 120);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(260, 129);
-            this.textBox2.TabIndex = 2;
+            this.txtDefintion.Location = new System.Drawing.Point(12, 120);
+            this.txtDefintion.Multiline = true;
+            this.txtDefintion.Name = "txtDefintion";
+            this.txtDefintion.Size = new System.Drawing.Size(260, 244);
+            this.txtDefintion.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(32, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(112, 13);
             this.label1.TabIndex = 3;
-            this.label1.Text = "label1";
+            this.label1.Text = "Insert Word To Define";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(12, 104);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(56, 13);
             this.label2.TabIndex = 4;
-            this.label2.Text = "label2";
+            this.label2.Text = "Definitions";
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(12, 370);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(75, 23);
+            this.btnPrevious.TabIndex = 5;
+            this.btnPrevious.Text = "Previous";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(197, 370);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 6;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // labelPageNum
+            // 
+            this.labelPageNum.AutoSize = true;
+            this.labelPageNum.Location = new System.Drawing.Point(122, 375);
+            this.labelPageNum.Name = "labelPageNum";
+            this.labelPageNum.Size = new System.Drawing.Size(36, 13);
+            this.labelPageNum.TabIndex = 7;
+            this.labelPageNum.Text = "( 0/0 )";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(284, 405);
+            this.Controls.Add(this.labelPageNum);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrevious);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtDefintion);
+            this.Controls.Add(this.txtWord);
+            this.Controls.Add(this.btnGetDefinition);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -96,11 +132,14 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnGetDefinition;
+        private System.Windows.Forms.TextBox txtWord;
+        private System.Windows.Forms.TextBox txtDefintion;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Label labelPageNum;
     }
 }
 
--- SOAP-Dictionary_Service/SOAP-Dictionary_Service/Form1.cs
+++ SOAP-Dictionary_Service/SOAP-Dictionary_Service/Form1.cs
@@ -7,14 +7,82 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SOAP_Dictionary_Service.DictionaryService;
+using System.Net;
 
 namespace SOAP_Dictionary_Service
 {
     public partial class Form1 : Form
     {
+        int totalDef = -1;
+        int currDef = 0;
+        SOAP_Dictionary_Service.DictionaryService.Definition[] definitions;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnGetDefinition_Click(object sender, EventArgs e)
+        {
+            String word = txtWord.Text;
+
+           DictionaryService.DictService service = new DictionaryService.DictService();
+
+            WordDefinition defintion = service.Define(word);
+
+            if (defintion.Definitions.Length == 0)
+            {
+                txtDefintion.Text = "No definition for " + word;
+            }
+            else
+            {
+                totalDef = defintion.Definitions.Count();
+                definitions = new SOAP_Dictionary_Service.DictionaryService.Definition [totalDef + 1] ;
+                currDef = 0;
+                for(int i = 0; i < totalDef; i++)
+                {
+                    definitions[i] = defintion.Definitions[i];
+                }
+
+                txtDefintion.Text = "" + definitions[currDef].ToString();
+                labelPageNum.Text = (currDef + 1) + "/" + totalDef;
+            }
+
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (totalDef > 0)
+            {
+                if (currDef == totalDef - 1)
+                {
+                    currDef = 0;
+                }
+                else
+                {
+                    currDef = currDef + 1;
+                }
+                txtDefintion.Text = "" + definitions[currDef];
+                labelPageNum.Text = (currDef + 1) + "/" + totalDef;
+            }
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            if (totalDef > 0)
+            {
+                if (currDef == 0)
+                {
+                    currDef = totalDef - 1;
+                }
+                else
+                {
+                    currDef = currDef - 1;
+                }
+                txtDefintion.Text = "" + definitions[currDef];
+                labelPageNum.Text = (currDef + 1) + "/" + totalDef;
+            }
         }
     }
 }
